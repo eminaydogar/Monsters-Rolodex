22feb9e09fc14ab98fcd286645c230f3376d7681
eminaydogar <38189786+eminaydogar@users.noreply.github.com>
2019-11-23 20:06:03 +0300

    Updates

---
+++ precache-manifest.2d1f1852a5d4f63fe75083c0c00a765d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845e5e87e30f88e56204e6a744248f06",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "5b89256de8fdc2f76e0d",
+    "url": "/Monsters-Rolodex/static/css/main.1d45ec73.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/Monsters-Rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "5b89256de8fdc2f76e0d",
+    "url": "/Monsters-Rolodex/static/js/main.56a716c2.chunk.js"
+  },
+  {
+    "revision": "11e2dea3cac39d985a42",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.3ef3660b.js"
+  }
+]);
